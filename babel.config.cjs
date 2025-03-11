069bc3cd8d3d67f675ec1a0ff579c964a98db5d0
Son nguyen <sonnguyen@seysolutions.com>
2025-03-11 16:03:18 +0700

    update babel

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current',
-          },
-        },
-      ],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: "> 0.25%, not dead, IE 11",
+        useBuiltIns: "entry",
+        corejs: 3,
+      },
     ],
-  };
+  ],
+};
