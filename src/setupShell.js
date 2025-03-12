c75389610e0fbc12febac88e13c27ce9ae8a9920
Son nguyen <sonnguyen@seysolutions.com>
2025-03-12 13:14:39 +0700

    release 2

--- src/setupShell.js
+++ src/setupShell.js
@@ -1,9 +1,17 @@
 import "./chatbot.js";
- function setupShell() {
-    let htmlString = `<chat-bot tenantId='demosolutions'></chat-bot>`;
-    const div = document.createElement('div');
+function setupShell() {
+  const scriptTag = document.getElementById("seychatbot");
+  if (scriptTag) {
+    const tenantId = scriptTag.getAttribute("data-tenantId");
+    console.log("Tender ID:", tenantId);
+
+    let htmlString = `<chat-bot tenantId='${tenantId}'></chat-bot>`;
+    const div = document.createElement("div");
     div.innerHTML = htmlString;
     document.body.appendChild(div);
+  } else {
+    console.error("Script tag with ID 'seychatbot' not found.");
+  }
 }
 
 setupShell();
